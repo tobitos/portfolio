865abb9cf27e1d203ea6b7bc7c73ad97cc65d382
Tobi Poel <tobipoel@gmail.com>
2017-08-26 20:39:44 +0200

    adjusts 404 page content

--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -17,37 +17,24 @@ import A from 'components/A';
 
 import messages from './messages';
 
-import Logo from './logolinks.png';
-
-const Img = styled.img`
-  width: 150px;
-  margin-top: 25px;
-  margin-left: 25px;
-  margin-right: 25px;
-`;
-
-const ImageContainer = styled.div`
-  text-align: right;
-  margin-bottom: 40px;
-`;
-
 const Title = styled.h1`
-  margin-left: 25px;
-  margin-right: 25px;
+  max-width: 982px;
+  margin: auto;
+  padding: 56px 24px 16px 24px;
   font-weight: 300;
   font-family: Helvetica, Arial, sans-serif;
   font-size: 32px;
   line-height: 40px;
   color: #333333;
-  max-width: 50%;
 `;
 
 const BackHome = styled(A)`
-  margin-left: 25px;
-  margin-right: 25px;
+  display: block;
+  max-width: 982px;
+  margin: auto;
+  padding: 8px 24px 16px 24px;
   font-weight: 300;
   font-family: Helvetica, Arial, sans-serif;
-  max-width: 50%;
 `;
 
 // eslint-disable-next-line react/prefer-stateless-function
@@ -55,9 +42,6 @@ export default class NotFound extends React.PureComponent {
   render() {
     return (
       <div>
-        <ImageContainer>
-          <Img src={Logo} />
-        </ImageContainer>
         <Title>
           <FormattedMessage {...messages.header} />
         </Title>
